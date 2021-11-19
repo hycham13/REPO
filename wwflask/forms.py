890bfaad74bdb18fcbceb9b0f8c46799a63bf3ca
hycham13 <94229036+hycham13@users.noreply.github.com>
2021-11-19 08:25:49 -0500

    Add files via upload

---
+++ wwflask/forms.py
@@ -0,0 +1,43 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed,FileRequired
+from wtforms import StringField,TextField,TextAreaField,IntegerField,DateField,SubmitField,FileField,SelectField
+from wtforms.validators import DataRequired,Email,ValidationError,Length
+from datetime import datetime
+from model import User
+
+class Unique(object):
+    def __init__(self, model, field, message=u'This element already exists.'):
+        # model = User objecte
+        # field = User.name or User.email
+        self.model = model
+        self.field = field
+        self.message = message
+    def __call__(self, form, field):
+        check = self.model.query.filter(self.field == field.data).first()
+        if check:
+            raise ValidationError(self.message)
+
+### Creat register form ###
+class Contact(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(),Unique(User,User.name),Length(min=3, max=20)])
+    email  = TextField('Email Adresse',validators=[DataRequired(),Email(),Unique(User,User.email)])
+    message = TextAreaField('Message',validators=[DataRequired(),Length(min=6, max=50)])
+
+
+###### login form #####
+class Validecontact(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = TextField('Email Adresse', validators=[DataRequired(), Email()])
+
+######   register profile cv #####
+class CvForm(FlaskForm):
+    picture = FileField('Pictures', validators=[FileRequired(),FileAllowed(['jpg', 'png','gif'], 'Images only!')])
+    date = DateField('Date',validators=[DataRequired()],render_kw = {'placeholder':'years-month-day'} )
+    fullname = StringField('FullName', validators=[DataRequired(), Unique(User, User.name), Length(min=3, max=20)])
+    email = TextField('Email Adresse', validators=[DataRequired(), Email(), Unique(User, User.email)])
+    phone = IntegerField('PhoneNumber',validators=[DataRequired(),Length(min=10,max=10)])
+    address = StringField('Adresse', validators=[DataRequired()]  )
+    location = SelectField('Location',validators =[DataRequired()],choices = [('location','select location'),('annaba','Annaba'),('algeria','Algeria')],)
+    computerskills = TextAreaField('Computerskills', validators=[DataRequired()],render_kw = {'placeholder':'entry all computer skills such as word,excel, programing language ...........etc'} )
+    objective = TextAreaField('Objective', validators=[DataRequired()],render_kw = {'placeholder' : 'entry objective'})
+    submit = SubmitField('Submit')
---
+++ wwflask/model.py
@@ -0,0 +1,74 @@
+from flask_sqlalchemy import SQLAlchemy
+
+from start import app
+
+db = SQLAlchemy(app)
+
+user  = 'postgres'
+pw   = 'admin'
+data = 'test'
+host = 'localhost'
+port = '5432'
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{pw}@{host}:{port}/{data}'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config ['SQLALCHEMY_RECORD_QUERIES'] = True
+
+
+### Creat Model for DB with SQlalshemy ###
+class User(db.Model):
+
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20),unique = True,nullable= False)
+    email = db.Column(db.String(30),unique = True , nullable = False)
+    message = db.Column(db.String(50),nullable = False)
+
+    def __init__(self,name,email,message):
+        self.name = name
+        self.email = email
+        self.message = message
+
+    def __repr__(self):
+        return f'Post {self.name}'
+
+
+#### create cv for user ####
+class Cv(db.Model):
+
+    __tablename__ = 'cv'
+    id = db.Column(db.Integer ,primary_key = True,nullable = False)
+    date = db.Column(db.String())
+    fullname = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    phone = db.Column(db.Integer(),  nullable=False)
+    address = db.Column(db.String(30), nullable=False)
+
+    computerskills = db.Column(db.String(50), nullable=False)
+    location = db.Column(db.String(50))
+    objective = db.Column(db.String(50), nullable=False)
+    #picture = db.Column(db.LargeBinary(500), nullable=False)
+    picture = db.Column(db.String() , nullable = False)
+
+    user = db.relationship('User', backref = db.backref('cvv',  lazy=True))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __init__(self,data,fullname,email,phone,address,location,computerskill,objective,picture,user):
+        self.date = data
+        self.fullname = fullname
+        self.email = email
+        self.phone = phone
+        self.address = address
+        self.location = location
+        self.computerskills = computerskill
+        self.objective = objective
+        self.picture = picture
+        self.user = user
+
+
+    def __repr__(self):
+        return f'Post {self.fullname}'
+###### leave comment and execute ########
+#db.drop_all()           
+db.create_all()
